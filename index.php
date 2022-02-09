a71a79a773e7bd942f14aaa052881b222ab97c5b
jseo123 <jose123135@gmail.com>
2022-02-10 00:30:09 +0100

    Fixed flex from main page

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once "libs/Database.php";
+require_once "libs/classes/view.php";
+require_once "libs/classes/model.php";
+require_once "libs/classes/controller.php";
+require_once "libs/app.php";
+require_once "config/constants.php";
+require_once "config/db.php";
+$app = new App();
